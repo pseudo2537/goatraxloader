3c1d116735d1f63de66f06c421977867333eaaaa
pseudo2537 <jonasuear@gmail.com>
2024-06-16 22:36:24 +0200

    fixed bug

--- goatraxloader.sh
+++ goatraxloader.sh
@@ -17,10 +17,27 @@ while IFS= read -r line; do
 	name_arr[$idx]=`echo ${str// /_}`
 done <<< $names
 
+
+unique=0
+declare -A dict
+
 idx=0
 while IFS= read -r line; do
-	echo $line
+	#echo $line
 	((sc=sc+1))
 	((idx=idx+1))
-	$(wget "$line" -O music/$dirname/"$sc_${name_arr[$idx]}.mp3")
+	s_name="$sc_${name_arr[$idx]}.mp3"
+	#check if same name already given and name not empty
+	if [[ $s_name == ".mp3" ]] || [[ dict[$s_name] -eq 1 ]]; then
+		echo "OK"
+		s_name="$unique$s_name"	
+		echo $s_name
+		((++unique))
+	fi	
+	$(wget "$line" -O music/$dirname/$s_name)
+	dict[$s_name]=1
+	#debug
+	#echo ${dict[$s_name]}
 done < musicurls
+
+echo ${dict[@]}
